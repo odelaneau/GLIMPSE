3b8c46cf5070cb16cf710460a573c4de934c011c
odelaneau <olivier.delaneau@gmail.com>
2020-01-18 12:23:20 +0100

    First LCC_sample version

---
+++ sample/src/sampler/sampler_algorithm.cpp
@@ -0,0 +1,134 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2018 Olivier Delaneau, University of Lausanne
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+////////////////////////////////////////////////////////////////////////////////
+#include <sampler/sampler_header.h>
+
+#define OFILE_VCFU	0
+#define OFILE_VCFC	1
+#define OFILE_BCFC	2
+
+#define GET(n,i)	(((n)>>(i))&1U)
+#define TOG(n,i)	((n)^=(1UL<<(i)))
+
+void sampler::sample() {
+	tac.clock();
+	string filename = options["input"].as < string > ();
+	vrb.title("Generating haplotype pairs for [" + filename + "]");
+
+	//Create all input file descriptors
+	bcf_srs_t * sr =  bcf_sr_init();
+	sr->collapse = COLLAPSE_NONE;
+	sr->require_index = 1;
+	if(!(bcf_sr_add_reader (sr, filename.c_str()))) vrb.error("Problem opening index file for [" + filename + "]");
+
+	//Extract sample IDs
+	vrb.bullet("Extract sample IDs");
+	int nsamples = bcf_hdr_nsamples(sr->readers[0].header);
+	vrb.bullet("#samples = " + stb.str(nsamples));
+
+	//Extract #pairs per sample
+	bcf_hrec_t * header_record = bcf_hdr_get_hrec(sr->readers[0].header, BCF_HL_GEN, "NMAIN", NULL, NULL);
+	if (header_record == NULL) vrb.error("Cannot retrieve NMAIN flag in VCF header");
+	int nmain = atoi(header_record->value);
+	if (nmain <1 || nmain > 16) vrb.error("NMAIN out of bounds : " + stb.str(nmain));
+	vrb.bullet("#main_iterations = " + stb.str(nmain));
+
+
+	//Generating task data
+	bool first_het = true;
+	bool maximize = !options.count("sample");
+	vector < int > sampled_conf = vector < int > (nsamples, -1);
+	vector < int > prev_HS = vector < int > (nsamples, 0);
+	vector < bool > prev_haps = vector < bool > (2*nsamples, false);
+	if (!maximize)  {
+		for (int i = 0 ; i < nsamples ; i ++)
+			sampled_conf[i] = rng.getInt(nmain);
+	}
+
+	//Create output file + header by duplication
+	string file_format = "w";
+	string fname = options["output"].as < string > ();
+	unsigned int file_type = OFILE_VCFU;
+	if (fname.size() > 6 && fname.substr(fname.size()-6) == "vcf.gz") { file_format = "wz"; file_type = OFILE_VCFC; }
+	if (fname.size() > 3 && fname.substr(fname.size()-3) == "bcf") { file_format = "wb"; file_type = OFILE_BCFC; }
+	htsFile * fp = hts_open(fname.c_str(),file_format.c_str());
+	bcf_hdr_t * hdr = bcf_hdr_dup(sr->readers[0].header);
+	bcf_hdr_write(fp, hdr);
+
+	//Main loop
+	int n_variants = 0;
+	int * buffer = NULL, nbuffer = 0, rbuffer = 0, *hs_fields = NULL, *gt_fields = NULL;
+
+	bcf1_t * line;
+	while (bcf_sr_next_line (sr)) {
+		//Retrieve variant informations
+		line =  bcf_sr_get_line(sr, 0);
+
+		//Retrive GT
+		int n_gt_fields = 0;
+		int ngt = bcf_get_genotypes(hdr, line, &gt_fields, &n_gt_fields);
+		assert(ngt == 2*nsamples && n_gt_fields == 2*nsamples);
+
+		//Retrieve HS
+		int n_hs_fields = 0;
+		int nhs = bcf_get_format_int32(hdr, line, "HS", &hs_fields, &n_hs_fields);
+		assert(nhs == nsamples && n_hs_fields == nsamples);
+
+		//Process & update record
+		if (maximize) {
+			for (int i = 0 ; i  < nsamples ; i ++) {
+				bool a0 = (bcf_gt_allele(gt_fields[2*i+0])==1);
+				bool a1 = (bcf_gt_allele(gt_fields[2*i+1])==1);
+				if (a0 != a1) {
+
+
+				}
+			}
+		} else {
+			for (int i = 0 ; i  < nsamples ; i ++) {
+				bool a0 = GET(hs_fields[i], 2*sampled_conf[i]+0);
+				bool a1 = GET(hs_fields[i], 2*sampled_conf[i]+1);
+				gt_fields[2*i+0] = bcf_gt_phased(a0);
+				gt_fields[2*i+1] = bcf_gt_phased(a1);
+			}
+			bcf_update_genotypes(hdr, line, gt_fields, nsamples*2);
+		}
+
+		//Clear other unwanted format field (all but GT)
+		bcf_update_format(hdr, line, "HS", NULL, 0, BCF_HT_INT);
+		bcf_update_format(hdr, line, "GP", NULL, 0, BCF_HT_INT);
+		bcf_update_format(hdr, line, "DS", NULL, 0, BCF_HT_INT);
+		// Write updated record in file
+		bcf_write1(fp, hdr, line);
+		//
+		n_variants++;
+	}
+	//Close file descriptors & free arrays
+	free(gt_fields);
+	free(hs_fields);
+	bcf_hdr_destroy(hdr);
+	bcf_sr_destroy(sr);
+	if (hts_close(fp)) vrb.error("Non zero status when closing VCF/BCF file descriptor");
+	//Last verbose
+	if (n_variants == 0) vrb.error("No variants to be phased in files");
+	vrb.title("Writing completed [L=" + stb.str(n_variants) + "] (" + stb.str(tac.rel_time()*1.0/1000, 2) + "s)");
+}
+
---
+++ sample/src/sampler/sampler_parameters.cpp
@@ -0,0 +1,93 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2018 Olivier Delaneau, University of Lausanne
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+////////////////////////////////////////////////////////////////////////////////
+
+#include <sampler/sampler_header.h>
+
+void sampler::declare_options() {
+	bpo::options_description opt_base ("Basic options");
+	opt_base.add_options()
+			("help", "Produce help message")
+			("seed", bpo::value<int>()->default_value(15052011), "Seed of the random number generator");
+
+	bpo::options_description opt_input ("Input files");
+	opt_input.add_options()
+			("input", bpo::value < string >(), "VCF/BCF generated through LCC_ligate");
+
+	bpo::options_description opt_algo ("Parameters");
+	opt_algo.add_options()
+			("sample", "Sample a likely haplotype pair for each sample, use it in conbination with --seed")
+			("solve", "Get the most likely haplotype pair for each sample, independent of --seed");
+
+	bpo::options_description opt_output ("Output files");
+	opt_output.add_options()
+			("output,O", bpo::value< string >(), "Output haplotype pairs in VCF/BCF format")
+			("log", bpo::value< string >(), "Log file");
+
+	descriptions.add(opt_base).add(opt_input).add(opt_algo).add(opt_output);
+}
+
+void sampler::parse_command_line(vector < string > & args) {
+	try {
+		bpo::store(bpo::command_line_parser(args).options(descriptions).run(), options);
+		bpo::notify(options);
+	} catch ( const boost::program_options::error& e ) { cerr << "Error parsing command line arguments: " << string(e.what()) << endl; exit(0); }
+
+
+	vrb.title("LCC_sample");
+	vrb.bullet("Author        : Simone RUBINACCI & Olivier DELANEAU, University of Lausanne");
+	vrb.bullet("Contact       : simone.rubinacci@unil.ch & olivier.delaneau@unil.ch");
+	vrb.bullet("Version       : 1.0.0");
+	vrb.bullet("Run date      : " + tac.date());
+
+	if (options.count("help")) { cout << descriptions << endl; exit(0); }
+
+	if (options.count("log") && !vrb.open_log(options["log"].as < string > ()))
+		vrb.error("Impossible to create log file [" + options["log"].as < string > () +"]");
+}
+
+void sampler::check_options() {
+	if (!options.count("input"))
+		vrb.error("You must specify the an input VCF/BCF to sample from using --input");
+
+	if ((options.count("sample") + options.count("solve")) != 1)
+		vrb.error("You must specify a mode to get hapltoypes using either --sample or --solve");
+
+	if (!options.count("output"))
+		vrb.error("You must specify an output VCF/BCF file with --output");
+
+	if (options.count("seed") && options["seed"].as < int > () < 0)
+		vrb.error("Random number generator needs a positive seed value");
+}
+
+void sampler::verbose_files() {
+	vrb.title("Files:");
+	vrb.bullet("Input VCF     : [" + options["input"].as < string > () + "]");
+	vrb.bullet("Output VCF    : [" + options["output"].as < string > () + "]");
+	if (options.count("log")) vrb.bullet("Output LOG    : [" + options["log"].as < string > () + "]");
+}
+
+void sampler::verbose_options() {
+	vrb.title("Parameters:");
+	vrb.bullet("Seed       : " + stb.str(options["seed"].as < int > ()));
+	if (options.count("sample")) vrb.bullet("Mode       : Get the most likely haplotype pair for each sample");
+	else vrb.bullet("Mode       : Sample a likely haplotype pair for each sample");
+}
