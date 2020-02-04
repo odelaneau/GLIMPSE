144c05c8146ece323e15e27fd5eb557372e8bf01
odelaneau <olivier.delaneau@gmail.com>
2020-02-04 01:10:03 +0100

    LCC_phase: new comments in haplotype_set

--- phase/src/containers/conditioning_set.h
+++ phase/src/containers/conditioning_set.h
@@ -49,8 +49,8 @@ public:
 	unsigned int n_sites;
 
 	//TRANSITION PROBABILITIES
-	vector < double > t;
-	vector < double > nt;
+	vector < float > t;
+	vector < float > nt;
 
 	//EMISSION
 	double ed;
@@ -95,47 +95,50 @@ public:
 		n_sites = 0;
 	}
 
+	// Update transition probabilities of the HMM for this particular conditioning set
 	void updateTransitions() {
-		t = vector < double > (Vpoly.size() - 1, 0.0);
-		nt = vector < double > (Vpoly.size() - 1, 0.0);
+		t = vector < float > (Vpoly.size() - 1, 0.0);
+		nt = vector < float > (Vpoly.size() - 1, 0.0);
 		for (int l = 1 ; l < Vpoly.size() ; l ++) {
-			double rho = 0.04 * n_effective * (mapG.vec_pos[Vpoly[l]]->cm - mapG.vec_pos[Vpoly[l-1]]->cm);
-			if (rho == 0.0) rho = 0.00001;
+			float distcm = mapG.vec_pos[Vpoly[l]]->cm - mapG.vec_pos[Vpoly[l-1]]->cm;
+			if (distcm < 0.00001f) distcm = 0.00001f;
+			float rho = 0.04 * n_effective * distcm;
 			t[l-1] = -1.0 * expm1(-1.0 * rho / n_haps);
 			nt[l-1] = 1-t[l-1];
 		}
 	}
 
+	// Build compact conditioning set, keeping track of polymorphic [need HMM compute] and monomorphic [no need of HMM compute] sites
 	void compact(int ind) {
 		clear();
 		n_states = idxH.size();
 		for (int l = 0 ; l < n_vars ; l ++) {
-			unsigned int ac = H.H_opt_var.get(l, 2*ind + H.n_ref + 0) + H.H_opt_var.get(l, 2*ind + H.n_ref + 1);
-			Hmono.push_back(H.H_opt_var.get(l,idxH[0]));
-			for (int k = 0 ; k < idxH.size() ; k ++) ac += H.H_opt_var.get(l,idxH[k]);
-			if (ac > 0 && ac < (idxH.size()+2)) {
-				Vpoly.push_back(l);
-				for (int k = 0 ; k < idxH.size() ; k ++) Hpoly.push_back(H.H_opt_var.get(l,idxH[k]));
-			} else Vmono.push_back(l);
+			unsigned int ac = H.H_opt_var.get(l, 2*ind + H.n_ref + 0) + H.H_opt_var.get(l, 2*ind + H.n_ref + 1);		// AC in target
+			Hmono.push_back(H.H_opt_var.get(l,idxH[0]));																// Allele for monomorphic
+			for (int k = 0 ; k < idxH.size() ; k ++) ac += H.H_opt_var.get(l,idxH[k]);									// AC in conditioning states
+			if (ac > 0 && ac < (idxH.size()+2)) {																		// Is the variant polymorphic? (using both cond. and target haps)
+				Vpoly.push_back(l);																						// If yes: store index
+				for (int k = 0 ; k < idxH.size() ; k ++) Hpoly.push_back(H.H_opt_var.get(l,idxH[k]));					// If yes: store alleles
+			} else Vmono.push_back(l);																					// If no: declare it as monomorphic
 		}
-		n_sites = Vpoly.size();
+		n_sites = Vpoly.size();		// Store number of polymorphic sites
 	}
 
+	// Select random set of reference haplotypes
 	void selectRandom(int ind, int K) {
 		//Selection
 		idxH.clear();
-		for (int h = 0 ; h < H.n_ref ; h ++) if (H.initializing_haps[h]) idxH.push_back(h);
+		for (int h = 0 ; h < H.n_ref ; h ++) if (H.initializing_haps[h]) idxH.push_back(h);		// The if is there when pooling is used
 		if (H.n_ref > K) {
 			random_shuffle(idxH.begin(), idxH.end());
 			idxH.erase(idxH.begin() + K, idxH.end());
 			sort(idxH.begin(), idxH.end());
 		}
-		//Compact it!
-		compact(ind);
-		// Update HMM parameters
-		updateTransitions();
+		compact(ind);	//Compact it!
+		updateTransitions();	// Update HMM parameters
 	}
 
+	// Select conditioning haplotypes using PBWT
 	void selectPBWT(int ind, int K) {
 		//Selection
 		idxH.clear();
@@ -148,10 +151,8 @@ public:
 			idxH.erase(idxH.begin() + K, idxH.end());
 			sort(idxH.begin(), idxH.end());
 		}
-		//Compact it!
-		compact(ind);
-		// Update HMM parameters
-		updateTransitions();
+		compact(ind);	//Compact it!
+		updateTransitions();	// Update HMM parameters
 	}
 };
 
