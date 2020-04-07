9f5e9ff7a43e3b30ee4b7282a34b89da8b4b4123
Simone Rubinacci <simone.rubinacci@unil.ch>
2020-04-07 12:42:26 +0200

    Effective diploid population size

--- phase/src/caller/caller_parameters.cpp
+++ phase/src/caller/caller_parameters.cpp
@@ -45,7 +45,8 @@ void caller::declare_options() {
 			("pbwt-depth", bpo::value<int>()->default_value(2), "Number of neighbors to store")
 			("pbwt-modulo", bpo::value<int>()->default_value(8), "Frequency of PBWT storage")
 			("init-states", bpo::value<int>()->default_value(1000), "Number of states used for initialization")
-			("init-pool", bpo::value< string >(), "Pool of samples from which initializing haplotypes should be chosen");
+			("init-pool", bpo::value< string >(), "Pool of samples from which initializing haplotypes should be chosen")
+			("ne", bpo::value<float>()->default_value(20000.0), "Effective diploid population size");
 
 
 	bpo::options_description opt_output ("Output files");
