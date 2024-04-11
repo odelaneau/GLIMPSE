61004a4eae602eceac2b5644ce6128779f500a72
Chris Kachulis <ckachuli@broadinstitute.org>
2024-04-11 10:27:22 -0400

    fix

--- phase/src/io/genotype_bam_caller.cpp
+++ phase/src/io/genotype_bam_caller.cpp
@@ -163,7 +163,7 @@ void genotype_bam_caller::call_mpileup(int i)
 	if ( !aux_data.hdr ) vrb.error("Failed to read header: " + mpileup_data.bam_fnames[i] + ".");
 
 	if (mpileup_data.bai_fnames.empty()) {
-		aux_data.idx = sam_index_load2(aux_data.fp, mpileup_data.bam_fnames[i].c_str(), mpileup_data.bai_fnames.c_str());
+		aux_data.idx = sam_index_load2(aux_data.fp, mpileup_data.bam_fnames[i].c_str(), mpileup_data.bai_fnames[i].c_str());
 	} else {
 		aux_data.idx = sam_index_load(aux_data.fp, mpileup_data.bam_fnames[i].c_str());
 	}
