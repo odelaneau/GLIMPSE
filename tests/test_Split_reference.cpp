33efc96d091725e27ac852a793c1ab2711572a6e
Crunk <96680644+Truongphi20@users.noreply.github.com>
2025-07-07 10:16:09 -0400

    update: implement test case for split ref

--- tests/test_Split_reference.cpp
+++ tests/test_Split_reference.cpp
@@ -2,7 +2,24 @@
 #include "TestFile.hpp"
 #include "caller_header.h"
 
+// Map folder
+#ifndef MAP_FOLDER
+#define MAP_FOLDER
+#endif
+
+namespace fs = std::filesystem;
+
 TEST(Split_reference, test_split_ref_file)
-{
-    
+{   
+    // Create posfile
+    fs::path pos_file = TestFile().get_tmp_vcf_file("simple_pos_file");
+
+    // Create args string
+    std::vector<std::string> args{
+        "--input", pos_file.string(),
+        "--region", "chr1",
+        "--output", "chunks.chr1.txt",
+        "--map", MAP_FOLDER + "/chr1.b38.gmap.gz"
+    };
+    caller().phase(args);
 }
